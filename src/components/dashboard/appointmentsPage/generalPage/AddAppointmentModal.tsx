04bdf14f807722e6d23affe8857979af7ae20f02
Jayb <ajiboyeabiodun001@gmail.com>
2023-10-11 23:49:07 +0100

    design implementation

--- src/components/dashboard/appointmentsPage/generalPage/AddAppointmentModal.tsx
+++ src/components/dashboard/appointmentsPage/generalPage/AddAppointmentModal.tsx
@@ -7,7 +7,6 @@ import CustomSelect from "../../../ui/customHTMLElements/CustomSelect";
 import { OutlineButton, PrimaryButton } from "../../../ui/Button copy/Button";
 import { dataQueryStatus } from "../../../../utils/dataQueryStatus";
 
-
 const { IDLE, LOADING, SUCCESS, ERROR, DATAMODE, NULLMODE } = dataQueryStatus;
 
 const AddAppointmentModal = ({
@@ -24,7 +23,7 @@ const AddAppointmentModal = ({
     formState: { errors },
   } = useForm();
 
-  const [details, setDetails] = useState({})
+  const [details, setDetails] = useState({});
 
   const handleChange = (target: any, name: any) => {
     setDetails({ ...details, [name]: target?.value });
@@ -34,7 +33,6 @@ const AddAppointmentModal = ({
     const { name, value } = e.target;
     setDetails({ ...details, [name]: value });
   };
-
 
   return (
     <ModalContainer closeModal={closeModal} showModal={showModal}>
@@ -58,7 +56,7 @@ const AddAppointmentModal = ({
         </div>
         <div className="w-full -mt-4">
           <p className="text-incoverGray pt-10 px-10 -mb-[20px] text-[18px] leading-[28px] font-[800]">
-            Claim Details
+            Appointment Details
           </p>
           <form
             className="pt-10 flex flex-col gap-[32px]"
@@ -66,41 +64,69 @@ const AddAppointmentModal = ({
           >
             <div className="px-10 flex flex-col gap-[32px]">
               <div className="grid grid-cols-2 gap-10 justify-between w-[100%] ">
-              <CustomInputField
+                <CustomInputField
                   type="text"
-                  label="Policy Number"
-                  errors={errors.policyNumber}
-                //   onChange={(e: any) => handleInputChange(e)}
-                  {...register("policyNumber")}
-                  //   defaultValue={policyNumber}
+                  label="Name"
+                  errors={errors.name}
+                  //   onChange={(e: any) => handleInputChange(e)}
+                  {...register("name")}
+                  //   defaultValue={name}
                 />
                 <CustomSelect
-                  placeholder="Claim Vehicle"
+                  placeholder="Doctor"
                   options={[]}
                   control={control}
+                  {...register("doctor")}
                   name="RegisteredVehicleInfoId"
-                //   defaultValue={details?.Id}
+                  //   defaultValue={details?.Id}
                   handleChange={(e, a) => handleChange(e, a)}
                 />
                 <CustomInputField
+                  type="text"
+                  label="Email"
+                  errors={errors.email}
+                  //   onChange={(e: any) => handleInputChange(e)}
+                  {...register("email")}
+                  //   defaultValue={email}
+                />
+                <CustomInputField
                   type="date"
-                  label="Date of Incident"
-                  name="DateOfAccident"
+                  label="Date"
+                  {...register("date")}
                   // min={new Date().toISOString().split("T")[0]}
                   onChange={(e: any) => handleInputChange(e)}
-                //   defaultValue={details?.DateOfIncidence}
+                  //   defaultValue={details?.DateOfIncidence}
                 />
                 <CustomInputField
                   type="time"
-                  label="Time of Accident"
-                  name="TimeOfAccident"
+                  label="Time From"
+                  {...register("timeFrom")}
                   onChange={(e: any) => handleInputChange(e)}
-                //   defaultValue={details.TimeOfAccident}
+                  //   defaultValue={details.timeFrom}
+                />
+                <CustomInputField
+                  type="time"
+                  label="Time To"
+                  {...register("timeTo")}
+                  onChange={(e: any) => handleInputChange(e)}
+                  //   defaultValue={details.timeTo}
+                />
+                <CustomInputField
+                  type="text"
+                  label="Phone Number"
+                  {...register("phoneNumber")}
+                  onChange={(e: any) => handleInputChange(e)}
+                  //   defaultValue={details.phoneNumber}
+                />
+                <CustomInputField
+                  type="text"
+                  label="Condition"
+                  {...register("condition")}
+                  onChange={(e: any) => handleInputChange(e)}
+                  //   defaultValue={details.condition}
                 />
               </div>
-              <div className="flex justify-between gap-[32px] w-[100%] ">
-                
-              </div>
+              <div className="flex justify-between gap-[32px] w-[100%] "></div>
             </div>
 
             <div className="mt-[23px] w-full px-[32px] border-solid border-0 border-t-[1px] border-incoverGrey py-[12px] sticky bottom-0 bg-white z-50">
@@ -117,11 +143,7 @@ const AddAppointmentModal = ({
                   title="Submit"
                   className="text-center align-middle w-[256px] py-[12px]"
                   loader={status === LOADING ? true : false}
-                  disabled={
-                    status === LOADING
-                      ? true
-                      : false
-                  }
+                  disabled={status === LOADING ? true : false}
                   // onClick={handleSubmit(onSubmit)}
                 />
               </div>
